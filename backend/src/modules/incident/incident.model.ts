b389bfbf47588dfe604590941c2458f7c4144c66
Aakash <editsky597@gmail.com>
2026-03-22 22:11:37 +0530

    add incident model with schema, indexes, and type definitions

---
+++ backend/src/modules/incident/incident.model.ts
@@ -0,0 +1,51 @@
+import mongoose, { Schema, type InferSchemaType } from "mongoose";
+
+const incidentSchema = new Schema(
+    {
+        endpointId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Endpoint",
+            required: true
+        },
+
+        userId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true
+        },
+
+        status: {
+            type: String,
+            enum: ["open", "resolved"],
+            default: "open"
+        },
+
+        startedAt: {
+            type: Date,
+            default: Date.now
+        },
+
+        resolvedAt: Date,
+
+        failureCount: {
+            type: Number,
+            default: 3
+        }, // Number of failed request after failure 
+
+    }, { timestamps: true });
+
+// indexes
+incidentSchema.index(
+    { endpointId: 1, status: 1 },
+    { unique: true, partialFilterExpression: { status: "open" } }
+); // Ensure only one open incident per endpoint
+
+incidentSchema.index({ userId: 1, createdAt: -1 });
+incidentSchema.index({ endpointId: 1, createdAt: -1 });
+
+
+// Types
+export type IncidentDocument = InferSchemaType<typeof incidentSchema>;
+
+// Export
+export const Incident = mongoose.model<IncidentDocument>("Incident", incidentSchema);
