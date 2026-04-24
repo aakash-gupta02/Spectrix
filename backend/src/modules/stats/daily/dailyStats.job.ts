10dc4ebcecefab8cdb6126c26ca24759932020cb
Aakash Gupta <editsky597@gmail.com>
2026-04-24 20:25:22 +0530

    add cron & implement for daily aggregation

---
+++ backend/src/modules/stats/daily/dailyStats.job.ts
@@ -0,0 +1,30 @@
+import cron from "node-cron";
+import { logger } from "../../../config/logger.js";
+import { aggregateDailyStats } from "../stats.service.js";
+
+let isRunning = false;
+
+export function startDailyStatsJob() {
+  cron.schedule(
+    "0 1 * * *", // Daily at 1:00 AM
+    async () => {
+      if (isRunning) {
+        logger.warn("[cron] Skipping, already running");
+        return;
+      }
+
+      isRunning = true;
+
+      try {
+        await aggregateDailyStats();
+      } catch (err) {
+        logger.error("[cron] Aggregation failed", err);
+      } finally {
+        isRunning = false;
+      }
+    },
+    {
+      timezone: "UTC",
+    },
+  );
+}
