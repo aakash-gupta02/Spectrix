15e76fbf951a7efdd27f9bc48e3af50b560ab41a
aakash-gupta02 <editsky597@gmail.com>
2026-07-11 20:46:16 +0530

    feat(incident): add update incident functionality and enhance incident model

--- backend/src/modules/incident/incident.route.ts
+++ backend/src/modules/incident/incident.route.ts
@@ -1,11 +1,29 @@
 import { Router } from "express";
-import { getIncidentById, getIncidents } from "./incident.controller.js";
-import { validateParams, validateQuery } from "../../core/middlewares/validateRequest.middleware.js";
-import { getIncidentsQuerySchema, incidentIdParamsSchema } from "./incident.validation.js";
+import {
+  getIncidentById,
+  getIncidents,
+  updateIncidentById,
+} from "./incident.controller.js";
+import {
+  validateBody,
+  validateParams,
+  validateQuery,
+} from "../../core/middlewares/validateRequest.middleware.js";
+import {
+  getIncidentsQuerySchema,
+  incidentIdParamsSchema,
+  updateIncidentSchema,
+} from "./incident.validation.js";
 
 const router = Router();
 
 router.get("/", validateQuery(getIncidentsQuerySchema), getIncidents);
 router.get("/:id", validateParams(incidentIdParamsSchema), getIncidentById);
+router.patch(
+  "/:id",
+  validateBody(updateIncidentSchema),
+  validateParams(incidentIdParamsSchema),
+  updateIncidentById,
+);
 
 export default router;
