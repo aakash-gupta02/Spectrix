83d636030e0032d5ed4774f28615cbb0ea3ed4bf
aakash-gupta02 <editsky597@gmail.com>
2026-07-11 16:41:59 +0530

    refactor(project-structure): move cron & worker into core & update imports

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,8 +2,8 @@ import app from "./app.js";
 import { connectDB } from "./core/config/db.js";
 import { env } from "./core/config/env.js";
 import { logger } from "./core/config/logger.js";
-import { startCronJobsIfEnabled } from "./cron/cron.start.js";
-import { startWorkerIfEnabled } from "./workers/monitor.worker.js";
+import { startCronJobsIfEnabled } from "./core/cron/cron.start.js";
+import { startWorkerIfEnabled } from "./core/workers/monitor.worker.js";
 
 const startServer = async (): Promise<void> => {
   await connectDB();
